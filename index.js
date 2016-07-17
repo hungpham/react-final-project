18cdb36ce664179f6794c2379b81feca75617547
Hung.Phamtan <hung.phamtan@pyramid-consulting.com>
2016-07-17 21:43:16 +0700

    using redux middleware for logger
    logging every action to know:
    - previous state
    - current action
    - next state

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ import App from './components/App';
 
 let store = createStore(todoApp);
 
+let next = store.dispatch;
+store.dispatch = function dispatchAndLog(action) {
+  console.group();
+  console.info('logger');
+  console.log('previous state:');
+  console.log(store.getState());
+  console.log('current action:', action.type);
+  console.log(action);
+  let result = next(action);
+  console.log('next state:');
+  console.log(store.getState());
+  console.groupEnd();
+  return result;
+}
+
 render(
   <Provider store={store}>
     <App />
